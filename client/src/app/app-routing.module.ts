a179eff3e6be1bea6eb760ef109ca25274c87688
Lucija Tatarević <lucija.tatarevic@gmail.com>
2019-08-25 23:37:43 +0200

    Add BG animation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './components/welcome/welcome.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '**', redirectTo: 'welcome' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
