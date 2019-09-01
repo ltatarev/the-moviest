04ea3fc592642fcf5eaacfbf05b0bc458c3b6e4c
Lucija Tatarević <lucija.tatarevic@gmail.com>
2019-09-01 15:03:11 +0200

    Add new components

---
+++ client/src/app/components/watchlists/watchlists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watchlists',
+  templateUrl: './watchlists.component.html',
+  styleUrls: ['./watchlists.component.css']
+})
+export class WatchlistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
