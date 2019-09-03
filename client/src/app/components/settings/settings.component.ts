ed5071826aaa77fdb6e50aabcb5a755806f96ac0
Lucija Tatarević <38048916+ltatarev@users.noreply.github.com>
2019-09-03 14:29:54 +0200

    Update settings.component.ts

--- client/src/app/components/settings/settings.component.ts
+++ client/src/app/components/settings/settings.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+    // router, formBuilder, userService, activated route (?) (or token)
   constructor() { }
 
   ngOnInit() {
+      // http get current profile data, display where needed
+      
   }
 
+    shuffleAvatar(currentId){
+    // function assigns new avatar id, different than current one
+    let newId = this.randomInt();
+        if (newId===currentId){
+        shuffleAvatar(currentId)
+        }
+        else {
+            return newId < 10 ? "0"+newId.toString() : newId.toString();
+        }
+    }
+    
+    randomInt(){
+        // random Int between 1 and 34 (number of current avatars)
+        return Math.floor(Math.random() * (34)) + 1;
+    }
+    
+    
 }
