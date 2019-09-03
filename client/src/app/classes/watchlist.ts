f92a4db324c9b56040ab4c401ad651d7452707a0
Lucija Tatarević <38048916+ltatarev@users.noreply.github.com>
2019-09-03 15:01:25 +0200

    Create watchlist.ts

---
+++ client/src/app/classes/watchlist.ts
@@ -0,0 +1,9 @@
+export class Watchlist {
+    constructor(
+        public _id: string,
+        public email: string,
+        public username: string,
+        public password: string,
+        public avatarURL: string
+    ) {}
+}
