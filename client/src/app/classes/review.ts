f6f06fb7e2f1309925ab6a6da072e64d6d06b52d
Lucija Tatarević <38048916+ltatarev@users.noreply.github.com>
2019-09-03 15:01:38 +0200

    Create review.ts

---
+++ client/src/app/classes/review.ts
@@ -0,0 +1,9 @@
+export class Review {
+    constructor(
+        public _id: string,
+        public email: string,
+        public username: string,
+        public password: string,
+        public avatarURL: string
+    ) {}
+}
