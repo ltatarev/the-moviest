7ead023e98cf4e113b67ea566f219574ee823f08
Lucija Tatarević <lucija.tatarevic@gmail.com>
2019-09-06 16:24:44 +0200

    Profile comp. update

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from "../../services/user.service"
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: "app-navbar",
+    templateUrl: "./navbar.component.html",
+    styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+    constructor(private userService: UserService) {}
 
-  constructor(private userService: UserService) { }
+    public profileAvatar: any;
+    public id: any;
 
-  public profileAvatar:any;
+    ngOnInit() {
+        this.profileAvatar = this.getCoverLink();
+        this.id = this.userService.user.value._id;
+    }
 
-  ngOnInit() {
-    this.profileAvatar = this.getCoverLink();
-  }
+    public getCoverLink() {
+        return this.userService.user.value.avatarURL;
+    }
 
-  public getCoverLink() {
-   return this.userService.user.value.avatarURL;
-  }
-    
-  navbarOpen = false;
+    navbarOpen = false;
 
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
+    toggleNavbar() {
+        this.navbarOpen = !this.navbarOpen;
+    }
 
-  logOut() {
-    return this.userService.logOut();
-  }
+    logOut() {
+        return this.userService.logOut();
+    }
 }
--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,42 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from "../../services/user.service"
-import { Observable } from 'rxjs';
-import { User } from 'src/app/classes/user';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { Observable } from "rxjs";
+import { User } from "src/app/classes/user";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: "app-profile",
+    templateUrl: "./profile.component.html",
+    styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
+    public user: any;
+    public username: any;
+    public bio: any;
+    public avatarURL: any;
+    public currentlyWatching: any;
+    public favorites: any;
 
-  public user: any;
-  public username: any;
-  public bio: any;
-  public avatarURL: any;
-  public currentlyWatching: any;
-  public favorites: any;
+    constructor(private userService: UserService) {}
 
-  constructor(private userService: UserService) { }
+    ngOnInit() {
+        this.favorites = { movie: "", tvShow: "", actor: "", genre: "" };
+        this.getProfileData().subscribe(
+            (res: any) => {
+                this.user = res.user;
+            },
+            err => console.error(err),
+            () => {
+                this.fillProfile(this.user);
+            }
+        );
+    }
 
-  ngOnInit() {
-    this.getProfileData().subscribe((res: any) => {
-      this.user = res.user;
-    },
-      (err) => console.error(err),
-      () => { this.fillProfile(this.user) });
-  }
+    private fillProfile(user) {
+        this.username = user.username;
+        this.bio = user.bio;
+        this.avatarURL = user.avatarURL;
+        this.currentlyWatching = user.currentlyWatching;
+        this.favorites = user.favorites;
+    }
 
-  private fillProfile(user) {
-    this.username = user.username;
-    this.bio = user.bio;
-    this.avatarURL = user.avatarURL;
-    this.currentlyWatching = user.currentlyWatching;
-    this.favorites = user.favorites;
-  }
-
-  private getProfileData() {
-    return this.userService.getProfileData(this.userService.user.value._id);
-  }
-
+    private getProfileData() {
+        return this.userService.getProfileData(this.userService.user.value._id);
+    }
 }
