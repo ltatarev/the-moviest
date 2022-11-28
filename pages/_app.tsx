7c83cb28ac2b34d5758f4ffafc35fc6c0506fdb4
ltatarev <lucija.tatarevic@gmail.com>
2022-11-29 00:46:40 +0100

    Add default font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { DM_Sans } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const dmSans = DM_Sans({
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  variable: "--font-dmSans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${dmSans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
