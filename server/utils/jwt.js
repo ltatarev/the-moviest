dcdf440c1297eb5113c6d7b44fa5e9317381884e
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-11-28 18:57:18 +0100

    Update jwt.js

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -3,6 +3,6 @@ const jwt = require('express-jwt');
 const secret = "QccG2VSXbmkWbWSW";
 const userProperty = "payload";
 
-const authMiddleware = jwt({ secret, userProperty });
+const authMiddleware = jwt({ secret, userProperty, algorithms: ['sha1', 'RS256', 'HS256'], });
 
 module.exports = { authMiddleware, secret };
